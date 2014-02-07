8a5937f4241179e74ffa4cc348161fc55ea190bc
Ovid <curtis_ovid_poe@yahoo.com>
2014-02-07 12:54:19 +0100

    A Torn chrome extension for prev/next characters

---
+++ prevnext.js
@@ -0,0 +1,57 @@
+var prevNext = {
+  makeLinks: function(baseURL, param) {
+    var self = this;
+    chrome.tabs.query( {'active': true}, function(tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        var regex = new RegExp("[\\?&]" + param + "=([^&#]*)");
+        current_id = regex.exec(url);
+        current_id = current_id == null ? "" : parseInt(decodeURIComponent(current_id[1].replace(/\+/g, " ")));
+        
+        if (current_id) {
+          self.buildPrevNext(tab, baseURL, current_id);
+        }
+        else {
+          var p = document.createElement('p');
+          p.innerHTML = 'Not on character page';
+          document.body.appendChild(p);
+        }
+    });
+  },
+  buildPrevNext: function(tab, baseURL, current_id) {
+    var self = this;
+    if ( link = document.getElementById('prev') ) {
+      link.parentNode.removeChild(link);
+    }
+    if ( link = document.getElementById('next') ) {
+      link.parentNode.removeChild(link);
+    }
+    var prevLink = baseURL + (current_id - 1);
+    var nextLink = baseURL + (current_id + 1);
+ 
+    var prev = document.createElement('a');
+    prev.setAttribute('href', "#");
+    prev.setAttribute('id',   'prev');
+    prev.addEventListener('click', function() {
+      chrome.tabs.update(tab.id, { "url":prevLink });
+      self.buildPrevNext(tab, baseURL, current_id - 1);
+    });
+    prev.innerHTML = '[ Previous ]';
+ 
+    var next = document.createElement('a');
+    next.setAttribute('href', "#");
+    next.setAttribute('id',   'next');
+    next.addEventListener('click', function() {
+      chrome.tabs.update(tab.id, { "url":nextLink });
+      self.buildPrevNext(tab, baseURL, current_id + 1);
+    });
+    next.innerHTML = '[ Next ]';
+ 
+    document.body.appendChild(prev);
+    document.body.appendChild(next);
+  },
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  prevNext.makeLinks('http://www.torn.com/profiles.php?XID=', 'XID');
+});
