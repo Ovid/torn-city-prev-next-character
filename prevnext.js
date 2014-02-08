aab81a763326ddec602b1a414fe04eae82a20d87
Ovid <curtis_ovid_poe@yahoo.com>
2014-02-08 17:29:34 +0100

    _addLink didn't add the link, so renamed it to _createLink

--- prevnext.js
+++ prevnext.js
@@ -1,13 +1,14 @@
 function prevNext(baseURL, param) {
+    // attributes
     this.baseURL = baseURL;
     this.param   = param;
 
     // methods
-    this.showPrevNextInPopup      = showPrevNextInPopup;
-    this._addLinks      = _addLinks;
-    this._addLink       = _addLink;
-    this._removeElement = _removeElement;
-    this._getContainer  = _getContainer;
+    this.showPrevNextInPopup = showPrevNextInPopup;
+    this._addLinks           = _addLinks;
+    this._createLink         = _createLink;
+    this._removeElement      = _removeElement;
+    this._getContainer       = _getContainer;
 
     function showPrevNextInPopup() {
         var self     = this; // because 'this' in the query won't refer to parent
@@ -34,17 +35,20 @@ function prevNext(baseURL, param) {
     function _addLinks(current_id) {
         this._removeElement('prev');
         this._removeElement('next');
-        var prev = this._addLink('prev', '[ Previous ]', current_id - 1);
-        var next = this._addLink('next', '[ Next ]',     current_id + 1);
+
+        var prev = this._createLink('prev', '[ Previous ]', current_id - 1);
+        var next = this._createLink('next', '[ Next ]',     current_id + 1);
 
         var container = this._getContainer();
         container.appendChild(prev);
         container.appendChild(next);
     }
 
+    // the container where our prev/next links will go
     function _getContainer() {
         return document.getElementById('links');
     }
+
     // given the id of an element, removes that element if it exists
     function _removeElement(id_name) {
         if ( element = document.getElementById(id_name) ) {
@@ -53,16 +57,16 @@ function prevNext(baseURL, param) {
     }
 
     // build an individual [previous] or [next] link
-    function _addLink(id_name, html, current_id) {
-        var linkTarget = this.baseURL + current_id;
-        var link       = document.createElement('a');
-        var self       = this;
-        var tab        = self.activeTab;
+    function _createLink(id_name, html, current_id) {
+        var url  = this.baseURL + current_id;
+        var link = document.createElement('a');
+        var tab  = this.activeTab;
+        var self = this;
 
         link.setAttribute('href', "#");
         link.setAttribute('id',   id_name);
         link.addEventListener('click', function() {
-            chrome.tabs.update(tab.id, { "url":linkTarget });
+            chrome.tabs.update(tab.id, { "url":url });
             self._addLinks(current_id);
         });
         link.innerHTML = html;
